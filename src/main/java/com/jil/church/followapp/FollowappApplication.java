3841e2ea6056f3f2f8a4791a10e08fec4637c0ef
Cris Villablanca <chrisvblanca@gmail.com>
2020-04-16 14:51:29 +0800

    Initial Commit

---
+++ src/main/java/com/jil/church/followapp/FollowappApplication.java
@@ -0,0 +1,13 @@
+package com.jil.church.followapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jil/church/followapp/FollowappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jil.church.followapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
